b009ff7c0df7771c23881e8e4585e10a23e05815
anaesta2023 <135604309+anaesta2023@users.noreply.github.com>
2023-06-17 14:33:16 +0530

    Add files via upload

---
+++ myfirstprograme.py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/dell/AppData/Local/Programs/Python/Python311/my first programe.py
+Hello,world!
+>>> 
+= RESTART: C:/Users/dell/AppData/Local/Programs/Python/Python311/my first programe.py
+Hello,world!
+what is your name?Anaesta
+Hello Anaesta
+>>> age=12
+>>> print(age)
+12
+>>> weight=40
+>>> print(weight)
+40
+>>> height=151
+>>> print(height)
+151
