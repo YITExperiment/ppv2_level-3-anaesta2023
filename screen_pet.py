83e0e64d2ecd55355e3a2d57f9d0d43cfba02362
anaesta2023 <135604309+anaesta2023@users.noreply.github.com>
2023-06-19 22:40:20 +0530

    Add files via upload

--- screen_pet.py
+++ screen_pet.py
@@ -29,16 +29,33 @@ eye_left=c.create_oval(130,110,160,170,outline='black',fill='white')
 pupil_left=c.create_oval(140,145,150,155,outline='black',fill='black')
 eye_right=c.create_oval(230,110,260,170,outline='black',fill='white')
 pupil_right=c.create_oval(240,145,250,155,outline='black',fill='black')
-mouth_normal=c.create_line(170,250,200,272,230,250,smooth=1,width=2,state=NORMAL)
+mouth_normal=c.create_line(170,250,200,272,230,250, smooth=1, width=2, state=NORMAL)
+
+mouth_happy=c.create_line(170,250,200,282,230,250, smooth=1, width=2, state=HIDDEN)
+mouth_sad=c.create_line(170,250,200,232,230,250, smooth=1, width=2, state=HIDDEN)
+cheek_left=c.create_oval(70,180,120,230, outline='pink', fill='pink', state=HIDDEN)
+cheek_right=c.create_oval(280,180,330,230, outline='pink', fill='pink', state=HIDDEN)
 
 c.pack()
 
 root.after(1000, blink)
-
+def show_happy(event):
+    if (20 <= event.x <= 350) and (20 <= event.y <= 350):
+        c.itemconfigure(cheek_left, state=NORMAL)
+        c.itemconfigure(cheek_right, state=NORMAL)
+        c.itemconfigure(mouth_happy, state=NORMAL)
+        c.itemconfigure(mouth_normal, state=HIDDEN)
+        c.itemconfigure(mouth_sad, state=HIDDEN)
+    return
+def hide_happy(event):
+    c.itemconfigure(cheek_left, state=HIDDEN)
+    c.itemconfigure(cheek_right, state=HIDDEN)
+    c.itemconfigure(mouth_happy, state=HIDDEN)
+    c.itemconfigure(mouth_normal, state=NORMAL)
+    c.itemconfigure(mouth_sad, state=HIDDEN)
+    return
+c.bind('<Motion>',show_happy)
+c.bind('<Leave>',hide_happy)
 root.mainloop()
 
 
-
-
-                            
-            
