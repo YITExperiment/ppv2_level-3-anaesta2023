c255dece675fc19fefefdb363dbfa2373351c0fb
anaesta2023 <135604309+anaesta2023@users.noreply.github.com>
2023-06-24 17:20:35 +0530

    Create Anasta.py
    completed level 3 puthiya payanangal

---
+++ Ana.py
@@ -0,0 +1,5 @@
+full name: PHILIP GNANARAVI ANAESTA SHANGHARINI
+grade : 7
+School: V/Rambaikulam Girls Maha Vidyalayam
+Telephone No: 0771980327
+Address: 11B, Josephvas lane, Rambaikulam, Vavuniya
